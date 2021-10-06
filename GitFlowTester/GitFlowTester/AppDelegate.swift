766befda919e4aa9bd0eef4c3f538f0322a1a6f9
Taras <chernyshtaras94@gmail.com>
2021-10-06 18:27:24 +0300

    feature 2 commit 3

--- GitFlowTester/GitFlowTester/AppDelegate.swift
+++ GitFlowTester/GitFlowTester/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         print("hello f2")
+        print("hello f2")
         return true
     }
 
